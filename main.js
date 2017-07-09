c4fdeba764993d66f2dd648e3eef25e4ecff1c2b
Mitchel Severe <truth531@gmail.com>
2017-07-09 13:12:12 -0400

    Added win logic

--- main.js
+++ main.js
@@ -1,44 +1,117 @@
-console.log('Let the games begin!');
-
-//   // Listen For Key Events
-//   document.addEventListener('click', function game(event) {
-//     console.log(event.keyCode);
-//     if (event.keyCode === 39) {
-//       playerOnePosition += 30;
-//       playerOne.style.left = `${playerOnePosition}px`;
-//     } else if (event.keyCode === 40) {
-//        playerTwoPosition += 30;
-//        playerTwo.style.left = `${playerTwoPosition}px`;
-//     }
-
-function startGame() {
+    console.log('Let the games begin!');
+    
     const LANDING = document.querySelector('#landing');
     const GAME = document.querySelector('#game');
     const HEADER = document.querySelector('#header');
-    
-    LANDING.style.display = 'none';
-    GAME.style.display = 'block';
+    const GRID_ONE = document.querySelector('#one');
+    const GRID_TWO = document.querySelector('#two');
+    const GRID_THREE = document.querySelector('#three');
+    const GRID_FOUR = document.querySelector('#four');
+    const GRID_FIVE = document.querySelector('#five');
+    const GRID_SIX = document.querySelector('#six');
+    const GRID_SEVEN = document.querySelector('#seven');
+    const GRID_EIGHT = document.querySelector('#eight');
+    const GRID_NINE = document.querySelector('#nine');
 
-    const GRID_SQUARE = document.querySelectorAll('.grid-square');
-    let counter = 0;
+    let turn = 0;
     let playerOne = 'X';
     let playerTwo = 'O';
 
+let startGame = function() {
+    const GRID_SQUARE = document.querySelectorAll('.grid-square');
+
+    LANDING.style.display = 'none';
+    GAME.style.display = 'block';
+
+    HEADER.innerHTML = `Turn: ${playerOne}`
 
     for (let i = 0; i < GRID_SQUARE.length; i++) {
         GRID_SQUARE[i].addEventListener('click', function() {
-            if (counter % 2 === 0) {
+            if (turn % 2 === 0) {
                 this.innerHTML = playerOne;
-                counter++;
-                console.log(counter);
+                HEADER.innerHTML = `Turn: ${playerTwo}`
+                turn++;
+                win();
             } else {
                 this.innerHTML = playerTwo;
-                counter++;
-                console.log(counter);
+                HEADER.innerHTML = `Turn: ${playerOne}`
+                turn++;
+                win();
             }
         });
     }
+}
 
+let win = function() {
+    // Starting from GRID ONE
+    if (GRID_ONE.innerText != '') {
+        // Horizontal win from Grid One
+       if (GRID_ONE.innerText === GRID_TWO.innerText && GRID_TWO.innerText === GRID_THREE.innerText) {
+            console.log(`${turn} wins!`);
+            resetBoard();
+        // Vertical win from Grid One
+        } else if (GRID_ONE.innerText === GRID_FOUR.innerText && GRID_FOUR.innerText === GRID_SEVEN.innerText) {
+            console.log(`${turn} wins!`);
+            resetBoard();
+        // Diagonal win from Grid One
+        } else if (GRID_ONE.innerText === GRID_FIVE.innerText && GRID_FIVE.innerText === GRID_NINE.innerText) {
+            console.log(`${turn} wins!`);
+            resetBoard();
+        }
+    }
+
+    // Starting from GRID TWO
+    if (GRID_TWO.innerText != '') {
+        // Vertical win from Grid Two
+       if (GRID_TWO.innerText === GRID_FIVE.innerText && GRID_FIVE.innerText === GRID_EIGHT.innerText) {
+            console.log(`${turn} wins!`);
+            resetBoard();
+        }
+    }
+
+    // Starting from GRID THREE
+    if (GRID_THREE.innerText != '') {
+        // Vertical win from Grid Three
+       if (GRID_THREE.innerText === GRID_SIX.innerText && GRID_SIX.innerText === GRID_NINE.innerText) {
+            console.log(`${turn} wins!`);
+            resetBoard();
+        // Diagonal win from Grid Three
+        } else if (GRID_THREE.innerText === GRID_FIVE.innerText && GRID_FIVE.innerText === GRID_SEVEN.innerText) {
+            console.log(`${turn} wins!`);
+            resetBoard();
+        }
+    }
+
+    // Starting from GRID FOUR
+    if (GRID_FOUR.innerText != '') {
+        // Horizontal win from Grid Four
+       if (GRID_FOUR.innerText === GRID_FIVE.innerText && GRID_FIVE.innerText === GRID_SIX.innerText) {
+            console.log(`${turn} wins!`);
+            resetBoard();
+        }
+    }
+
+    // Starting from GRID SIX
+    if (GRID_SEVEN.innerText != '') {
+        // Horizontal win from Grid Six
+       if (GRID_SEVEN.innerText === GRID_EIGHT.innerText && GRID_EIGHT.innerText === GRID_NINE.innerText) {
+            console.log(`${turn} wins!`);
+            resetBoard();
+        }
+    }
+}
+
+let resetBoard = function() {
+    GRID_ONE.innerText = '';
+    GRID_TWO.innerText = '';
+    GRID_THREE.innerText = '';
+    GRID_FOUR.innerText = '';
+    GRID_FIVE.innerText = '';
+    GRID_SIX.innerText = '';
+    GRID_SEVEN.innerText = '';
+    GRID_EIGHT.innerText = '';
+    GRID_NINE.innerText = '';
+    turn = 0;
 }
 
 
